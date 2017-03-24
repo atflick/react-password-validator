64bf15d0e6be4838ed93bdefaf4decb0047db82d
Andy Flickinger <atflick@gmail.com>
2017-03-24 16:49:47 -0400

    submitting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      passwordConf: '',
+      validator: '',
+    }
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePassChange = this.handlePassChange.bind(this)
+    this.handlePassConfChange = this.handlePassConfChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.validator = this.validator.bind(this)
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value})
+  }
+
+  handlePassChange(event) {
+    this.setState({password: event.target.value})
+  }
+
+  handlePassConfChange(event) {
+    this.setState({passwordConf: event.target.value})
+  }
+
+  handleSubmit(event) {
+    this.validator()
+    event.preventDefault()
+  }
+
+  validator() {
+    if (this.state.password === this.state.passwordConf) {
+      this.setState({validator: "Valid"})
+    } else {
+      this.setState({validator: "Invalid"})
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>>
+          <label>Email</label>
+          <input type="text" onChange={this.handleEmailChange} />
+          <label>Password</label>
+          <input type="password" onChange={this.handlePassChange} />
+          <label>Confirm Password</label>
+          <input type="password" onChange={this.handlePassConfChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <label>{this.state.validator}</label>
       </div>
-    );
+
+    )
   }
 }
 
